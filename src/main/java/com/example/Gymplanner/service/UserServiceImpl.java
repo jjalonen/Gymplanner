604a24b9e7adaa1b69b055a42a7b3b1755356b19
jjalonen <joonas.jalonen@myy.haaga-helia.fi>
2019-05-07 13:31:53 +0300

    added postgres

--- src/main/java/com/example/Gymplanner/service/UserServiceImpl.java
+++ src/main/java/com/example/Gymplanner/service/UserServiceImpl.java
@@ -6,10 +6,8 @@ import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.AuthorityUtils;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
