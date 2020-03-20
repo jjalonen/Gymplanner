5e52f1e75d6dcb1d1e10351fd3134de9f2d0f848
jjalonen <45192669+jjalonen@users.noreply.github.com>
2020-03-20 14:32:52 +0200

    Update ExerciseRepository.java

--- src/main/java/com/example/Gymplanner/domain/ExerciseRepository.java
+++ src/main/java/com/example/Gymplanner/domain/ExerciseRepository.java
@@ -8,5 +8,4 @@ import com.example.Gymplanner.domain.Exercise;
 public interface ExerciseRepository extends CrudRepository<Exercise, Long>{
 	
 	List<Exercise> findByTitle(String title);
-    Exercise findBooksById(Long Id);
 }
